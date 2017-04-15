6aa3c31d38b4e7e208b553cf9a1f824ea7b870e5
Matthew <bennemat@seawolf.sonoma.edu>
2017-04-14 22:25:54 -0700

    Breaks on starting <stmt_list> wrong token

---
+++ TeamDP2/SyntacticalAnalyzer.h
@@ -0,0 +1,40 @@
+/********************************************************************************/
+/* Project: 									*/
+/* Author: 									*/
+/* Date: 									*/
+/* Description:									*/
+/********************************************************************************/
+
+#ifndef SYN_H
+#define SYN_H
+
+#include <iostream>
+#include <fstream>
+#include "LexicalAnalyzer.h"
+
+using namespace std;
+
+class SyntacticalAnalyzer 
+{
+    public:
+	SyntacticalAnalyzer (char * filename);
+	~SyntacticalAnalyzer ();
+    int define(int current_rule);
+    int param_list(int current_rule);
+    int else_part(int current_rule);
+    int stmt(int current_rule);
+    int stmt_list(int current_rule);
+    int literal(int current_rule);
+    int action(int current_rule);
+    int quoted_lit(int current_rule);
+    int any_other_token(int current_rule);
+    int more_tokens(int current_rule);
+    int more_defines(int current_rule);
+    private:
+	LexicalAnalyzer * lex;
+	ofstream p2file;
+	token_type token;
+	int program ();
+};
+	
+#endif
